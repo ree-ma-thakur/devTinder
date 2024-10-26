8792d8ab89540f72469cbec7980aed91dfac05e6
Reema Thakur <reemathakur2114@gmail.com>
2024-10-26 17:43:33 +0530

    get post delete http methofs vs use

---
+++ readme.md
@@ -0,0 +1,24 @@
+# Create Express server
+
+- Create a repository(create folder)
+- Initialize the repository(npm init)
+- node_modules, package.json, package-lock.json
+- Install express
+- Create a server
+- Listen to port 8080
+- Write request handlers for /test , /hello
+- Install nodemon and update scripts inside package.json
+- What are dependencies
+- What is the use of "-g" while npm install
+- Difference between caret and tilde ( ^ vs ~ )
+
+# Routing & Request Handlers
+
+- initialize git
+- gitignore
+- Create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extensions ex. /hello, / , hello/2, /xyz
+- Order of the routes matter a lot
+- Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on Postman
+-
--- src/app.js
+++ src/app.js
@@ -2,17 +2,24 @@ const express = require("express");
 
 const app = express();
 
-// handle the requests; request handler function; this takes route, callback function that send response
-app.use("/", (req, res) => {
-  res.send("Dashboard response");
+// .get() will match only get request for that mentioned route
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Reema" });
 });
 
+app.post("/user", (req, res) => {
+  // logic to save data to DB
+  res.send("Data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully");
+});
+
+// handle the requests; request handler function; this takes route, callback function that send response
+// .use() will match all http methods for that route mentioned
 app.use("/test", (req, res) => {
   res.send("Test from server");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hellooooo from server");
 });
 
 // listen to request at port, callback fun that will be called when server is up & running
