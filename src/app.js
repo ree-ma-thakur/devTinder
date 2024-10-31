11d3225cd62c4a31dd3ef167f8427ce26c599e36
Reema Thakur <reemathakur2114@gmail.com>
2024-10-31 17:11:05 +0530

    CRUD with APIs & Mongoose

--- src/app.js
+++ src/app.js
@@ -3,21 +3,66 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "Reema",
-    lastName: "Thakur",
-    email: "reema@gmail.com",
-    password: "Reema@123",
-  };
   // creating new instance of User model
-  const user = new User(userObj);
+  const user = new User(req.body);
   // Almost all mongoose function return promise therefore we have to use await to handle it
   try {
     await user.save();
     res.send("user addded successfully");
   } catch (err) {
     res.status(400).send("Error: " + err.message);
+  }
+});
+
+// Get user by email
+app.get("/user", async (req, res) => {
+  const email = req.body.email;
+  try {
+    // const users = await User.find({ email }); // find all user with this email
+    // if (users.length === 0) res.status(404).send("User not found");
+    // else res.send(users);
+    const user = await User.findOne({ email }); // find one 1st user with this email
+    if (!user) res.status(404).send("User not found");
+    else res.send(user);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find(); // find all users
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const user = await User.findByIdAndDelete(userId); // read documentation of findByIdAndDelete
+    res.send("User deleted");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ email: req.body.email }, data, {
+      // const user = await User.findByIdAndUpdate(userId, data, {
+      returnDocument: "after", // return the data that was before the update
+    });
+    console.log(user);
+    res.send("user updted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
   lastName: {
     type: String,
   },
-  emailIs: {
+  email: {
     type: String,
   },
   password: {
